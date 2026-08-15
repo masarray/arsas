093fa99ba14a1a7ea79cd07709e26942a5574047
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-15 13:27:53 +0700

    test: cover real SCL cross-LD DataSet completeness path

---
+++ tests/ARSAS.Tests/DataSetCompletenessDiagnosticTests.cs
@@ -0,0 +1,87 @@
+using System.Xml.Linq;
+using AR.Iec61850.Scl.Engineering;
+using ArIED61850Tester.Models;
+using ArIED61850Tester.Services;
+
+namespace ARSAS.Tests;
+
+public sealed class DataSetCompletenessDiagnosticTests
+{
+    [Fact]
+    public void SiemensLike_CrossLd_Fcda_Is_Canonical_Visible_And_Diagnostically_Complete()
+    {
+        var model = SclLiveModelProjectionBuilder.Build(
+            XDocument.Parse(CrossLogicalDeviceFixture()),
+            "Siprotec_cross_ld.cid");
+        var signals = new List<SignalDefinition>();
+
+        var merge = Iec61850DataSetSignalInventoryService.EnsureMandatorySignals(signals, model);
+        var snapshot = Iec61850DataSetCompletenessDiagnostic.Evaluate(model, signals);
+
+        Assert.Equal(1, merge.MandatoryCatalogCount);
+        var signal = Assert.Single(signals);
+        Assert.Equal("AA1C1F13R4ADD/GGIO6.CBOpnd", signal.ObjectReference);
+        Assert.False(signal.ObjectReference.EndsWith(".stVal", StringComparison.Ordinal));
+        Assert.DoesNotContain("[ST]", signal.ObjectReference);
+
+        Assert.Equal(1, snapshot.DataSetCount);
+        Assert.Equal(1, snapshot.StaticMemberCount);
+        Assert.Equal(1, snapshot.MandatoryInventoryCount);
+        Assert.Equal(1, snapshot.RepresentedCount);
+        Assert.Equal(0, snapshot.MissingCount);
+        Assert.True(snapshot.IsComplete);
+    }
+
+    [Fact]
+    public void Missing_Static_DataSet_Member_Is_Reported_With_DataSet_Index_And_Reference()
+    {
+        var model = SclLiveModelProjectionBuilder.Build(
+            XDocument.Parse(CrossLogicalDeviceFixture()),
+            "Siprotec_cross_ld.cid");
+
+        var snapshot = Iec61850DataSetCompletenessDiagnostic.Evaluate(
+            model,
+            Array.Empty<SignalDefinition>());
+
+        Assert.Equal(1, snapshot.MandatoryInventoryCount);
+        Assert.Equal(0, snapshot.RepresentedCount);
+        Assert.Equal(1, snapshot.MissingCount);
+        Assert.Contains("GGIO6.CBOpnd", snapshot.MissingReferences[0]);
+        Assert.Contains("[0]", snapshot.MissingReferences[0]);
+        Assert.False(snapshot.IsComplete);
+    }
+
+    private static string CrossLogicalDeviceFixture() => """
+    <SCL xmlns="http://www.iec.ch/61850/2003/SCL" version="2007" revision="B">
+      <Header id="SIEMENS_CROSS_LD" version="1" revision="0" />
+      <IED name="AA1C1F13R4">
+        <AccessPoint name="E">
+          <Server>
+            <LDevice inst="Application">
+              <LN0 lnClass="LLN0" lnType="LLN0Type">
+                <DataSet name="Digital">
+                  <FCDA ldInst="ADD" lnClass="GGIO" lnInst="6" doName="CBOpnd" fc="ST" />
+                </DataSet>
+              </LN0>
+            </LDevice>
+            <LDevice inst="ADD">
+              <LN0 lnClass="LLN0" lnType="LLN0Type" />
+              <LN lnClass="GGIO" inst="6" lnType="GGIOType" />
+            </LDevice>
+          </Server>
+        </AccessPoint>
+      </IED>
+      <DataTypeTemplates>
+        <LNodeType id="LLN0Type" lnClass="LLN0" />
+        <LNodeType id="GGIOType" lnClass="GGIO">
+          <DO name="CBOpnd" type="SpsType" />
+        </LNodeType>
+        <DOType id="SpsType" cdc="SPS">
+          <DA name="stVal" bType="BOOLEAN" fc="ST" />
+          <DA name="q" bType="Quality" fc="ST" />
+          <DA name="t" bType="Timestamp" fc="ST" />
+        </DOType>
+      </DataTypeTemplates>
+    </SCL>
+    """;
+}
