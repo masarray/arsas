1cc40eb5f59239f0393bf1194f3f2ef191ea1c69
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-09-08 02:37:35 +0700

    P1: use semantic signal names in FAT report

--- Services/IoTesting/IoFatV2ReportLayoutEngine.cs
+++ Services/IoTesting/IoFatV2ReportLayoutEngine.cs
@@ -189,7 +189,7 @@ internal static class IoFatV2ReportLayoutEngine
         var cells = new[]
         {
             rowNumber.ToString(),
-            Short(point.SignalName, 26),
+            Short(IoSignalDisplayName.Format(point.SignalName, point.ReportIecReference), 26),
             string.Empty,
             point.SignalKind.ToString(),
             ValueCell(point, FatValueSlot.Value1),
